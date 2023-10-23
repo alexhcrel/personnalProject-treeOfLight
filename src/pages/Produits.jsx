56917c4ef02edaa8c6d47f48ecaae02d947b1518
Alexandre Lerch <alexandre.lerch@yahoo.fr>
2023-10-23 13:42:35 +0200

    insertion footer dans page produit

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -2,15 +2,18 @@ import { useParams, useNavigate } from "react-router-dom";
 import React from "react";
 import Banner from "../components/Banner";
 import Exposition from "../components/Exposition";
+import Footer from "../components/Footer";
+import "../styles/produits.css";
 
 const Produits = () => {
 
   const numeroDeCategory = useParams();
   const objet = numeroDeCategory.produitCategory;
   return (
-    <div className="home">
+    <div className="produits">
       <Banner />
       <Exposition key={objet} objet={objet}/>
+      <Footer />
     </div>
   );
 };
